3e4b48f5cf1c1ffc90c91db84518853790a046d1
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-01-24 22:09:46 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk2Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //print out welcome message
+            Console.WriteLine("Welcome to Temperature Converter");
+            //print out the prompt to ask for temperature input
+            Console.WriteLine("Enter the temperature: ");
+            //collect the user input for temperature
+            float fahr = Convert.ToInt32(Console.ReadLine());
+
+            //calculates celsius using conversion formula
+            float cels = (fahr - 32) * 5/9;
+
+            //print out message saying what the perimeter is
+            Console.WriteLine($"{fahr}°F is equivalent to {cels}°F");
+
+            //pause screen to read output
+            Console.ReadLine();
+        }
+    }
+}
